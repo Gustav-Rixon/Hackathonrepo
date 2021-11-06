a6fef25f9e35d37514464e10e93abcf2fb1c4e4a
Gustav-Rixon <48015764+Gustav-Rixon@users.noreply.github.com>
2021-11-06 12:54:59 +0100

    memes

--- shit-poster/src/App.js
+++ shit-poster/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './meme0.gif';
 import './App.css';
 import React from 'react'
 import ReactPlayer from 'react-player'
@@ -7,23 +7,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          
-        >
-       
+        <img src={logo} className="App-logo" alt="logo" />      
         <ReactPlayer 
-        playing={true} 
         url='videos/RickAstley.mp4' 
+        playing={true}
+        muted={true}
         />
-        </a>
       </header>
     </div>
   );
