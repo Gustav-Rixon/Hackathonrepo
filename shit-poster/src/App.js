e267e4741ade69578d62ac9e08c50fbd7b450bfe
Gustav-Rixon <48015764+Gustav-Rixon@users.noreply.github.com>
2021-11-06 15:06:38 +0100

    mem

--- shit-poster/src/App.js
+++ shit-poster/src/App.js
@@ -1,18 +1,67 @@
-import logo from './meme0.gif';
+import meme from './meme/meme0.gif';
+import meme2 from './meme/5t6skv.jpg';
+import meme3 from './meme/edison.jpg';
 import './App.css';
 import React from 'react'
 import ReactPlayer from 'react-player'
+import axios from 'axios';
+import ReactSlider from 'react-slider';
+
+function getTheMeMe(){
+  console.log("helloe");
+  axios.get('./MemeIndex.json')
+  .then(res => {
+    //const path = res.path;
+    console.log('hi???????????????????????????????????????????????????+',res);
+  })
+}
+
+const openInNewTab = (url) => {
+  const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+  if (newWindow) newWindow.opener = null
+}
 
 function App() {
+
   return (
-    <div className="App">
+    <div className="App"> 
+
+<button onClick={() => openInNewTab('https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Foriginal%2F000%2F027%2F195%2Fcover10.jpg')}>
+  I like this site!
+</button>
+
+
+<button onClick={() => openInNewTab('https://cdn.discordapp.com/attachments/414542735866069008/901183687184494632/maxresdefault_1.jpg')}>
+  I dont like this site!
+</button>
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />      
+        <img src={meme3} className="App-logo2" alt="Manchester" /> 
+        <img src={meme} className="App-logo" alt="logo" />  
+        <img src={meme2} className="App-logo" alt="logo" />      
         <ReactPlayer 
         url='videos/RickAstley.mp4' 
         playing={true}
         muted={true}
         />
+
+    A very good slider yes!!
+    <ReactSlider
+      className="horizontal-slider"
+      thumbClassName="example-thumb"
+      trackClassName="example-track"
+      renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+      onSliderClick= {() => openInNewTab('https://www.youtube.com/watch?v=e4Kuwc88vZI')}
+    />
+    <ReactSlider
+    className="horizontal-slider"
+    thumbClassName="example-thumb"
+    trackClassName="example-track"
+    renderTrack={(props, state) => <div {...props} />}//custom track
+/>
+
+
       </header>
     </div>
   );
